ed71c1202ec1f47c1a7b70393da28a77a8bb89b2
Alph4 <alph4@gmx.at>
2025-04-21 22:57:04 +0200

    Working prototype

--- BingoAdvancedCustomGeneration/BingoAdvancedCustomGeneration.cs
+++ BingoAdvancedCustomGeneration/BingoAdvancedCustomGeneration.cs
@@ -1,4 +1,5 @@
 ﻿using Modding;
+using MonoMod.Utils;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -20,13 +21,14 @@ namespace BingoAdvancedCustomGeneration
             {
                 GoalManager.PreCopyGoalGroup(groupName);
             }
-            GoalManager.SetupCustomVanillaExclusions();
 
-            List<string> goals = ["Buy 6 map pins from Iselda (All but two)", "Buy 8 map pins from Iselda (All)", "Desolate Dive", "Soul Master", "Descending Dark", "Dream Nail", "Xero", "Crystal Heart", "Crystal Guardian 1", "Kill Myla", "Lumafly Lantern", "Unlock Queen's Stag + King's Stag Stations", "Have 1500 geo in the bank", "Abyss Shriek", "All Grubs: Greenpath (4) + Fungal (2)", "All Grubs: Xroads (5) + Fog Canyon (1)", "Break the 420 geo rock in Kingdom's Edge", "Broken Vessel", "Collect 3 King's Idols", "Collect 500 essence", "Collector", "Colosseum 1", "Complete 4 full dream trees", "Cyclone Slash", "Dash Slash", "Deep Focus + Quick Focus", "Defeat Colosseum Zote", "Dream Gate", "Dream Nail", "Dream Wielder", "Dung Defender", "Elder Hu", "Failed Champion", "False Knight + Brooding Mawlek"];
+            Dictionary<string, AdvancedGoal> goals = [];
+            goals.AddRange(GoalManager.GetGoalsByGroupName("Vanilla"));
+            goals.AddRange(GoalManager.GetGoalsByGroupName("Extended"));
+            
+            GoalManager.SetupCustomTournamentExclusions(goals);
 
-            Log(goals.Count);
-
-            BingoSync.Goals.AddGameMode(new AdvancedGameMode("Tournament", GoalManager.GetGoalsByName(goals)));
+            BingoSync.Goals.AddGameMode(new AdvancedGameMode("Tournament 3", goals));
 
             Log("Initializing");
         }
--- BingoAdvancedCustomGeneration/GoalManager.cs
+++ BingoAdvancedCustomGeneration/GoalManager.cs
@@ -23,36 +23,35 @@ namespace BingoAdvancedCustomGeneration
             goalGroups.Add(groupName, advancedGoals);
         }
 
-        public static void SetupCustomVanillaExclusions()
+        public static void SetupCustomTournamentExclusions(Dictionary<string, AdvancedGoal> goals)
         {
-            Dictionary<string, AdvancedGoal> advancedGoals = goalGroups["Vanilla"];
             bool lineExlusion = true;
             bool fullExclusion = false;
 
-            Exclude(advancedGoals, "Kill Myla", "Crystal Heart", fullExclusion);
+            Exclude(goals, "Kill Myla", "Crystal Heart", fullExclusion);
 
-            Exclude(advancedGoals, "Lumafly Lantern", "Kill Myla", lineExlusion);
-            Exclude(advancedGoals, "Lumafly Lantern", "Crystal Heart", lineExlusion);
-            Exclude(advancedGoals, "Lumafly Lantern", "Crystal Guardian 1", lineExlusion);
+            Exclude(goals, "Lumafly Lantern", "Kill Myla", lineExlusion);
+            Exclude(goals, "Lumafly Lantern", "Crystal Heart", lineExlusion);
+            Exclude(goals, "Lumafly Lantern", "Crystal Guardian 1", lineExlusion);
 
-            Exclude(advancedGoals, "Descending Dark", "Desolate Dive", lineExlusion);
-            Exclude(advancedGoals, "Descending Dark", "Soul Master", lineExlusion);
+            Exclude(goals, "Descending Dark", "Desolate Dive", lineExlusion);
+            Exclude(goals, "Descending Dark", "Soul Master", lineExlusion);
 
-            Exclude(advancedGoals, "Slash Zote's corpse in Greenpath", "Defeat Colosseum Zote", fullExclusion);
-            Exclude(advancedGoals, "Slash Zote's corpse in Greenpath", "Rescue Zote in Deepnest", fullExclusion);
-            Exclude(advancedGoals, "Slash Zote's corpse in Greenpath", "Vengefly King + Massive Moss Charger", fullExclusion);
+            Exclude(goals, "Slash Zote's corpse in Greenpath", "Defeat Colosseum Zote", fullExclusion);
+            Exclude(goals, "Slash Zote's corpse in Greenpath", "Rescue Zote in Deepnest", fullExclusion);
+            Exclude(goals, "Slash Zote's corpse in Greenpath", "Vengefly King + Massive Moss Charger", fullExclusion);
 
-            Exclude(advancedGoals, "Unlock Queen's Stag + King's Stag Stations", "Have 1500 geo in the bank", lineExlusion);
+            Exclude(goals, "Unlock Queen's Stag + King's Stag Stations", "Have 1500 geo in the bank", lineExlusion);
 
-            Unexclude(advancedGoals, "Save the 2 grubs in Hive", "Mask Shard  in the Hive");
-            Exclude(advancedGoals, "Save the 2 grubs in Hive", "Mask Shard  in the Hive", lineExlusion);
-            Exclude(advancedGoals, "Save the 2 grubs in Hive", "Hive Knight", lineExlusion);
-            Exclude(advancedGoals, "Save the 2 grubs in Hive", "Hiveblood", lineExlusion);
+            Unexclude(goals, "Save the 2 grubs in Hive", "Mask Shard  in the Hive");
+            Exclude(goals, "Save the 2 grubs in Hive", "Mask Shard  in the Hive", lineExlusion);
+            Exclude(goals, "Save the 2 grubs in Hive", "Hive Knight", lineExlusion);
+            Exclude(goals, "Save the 2 grubs in Hive", "Hiveblood", lineExlusion);
 
-            Unexclude(advancedGoals, "Tram Pass + Visit all 5 Tram Stations", "Hive Knight");
-            Unexclude(advancedGoals, "Tram Pass + Visit all 5 Tram Stations", "Hiveblood");
-            Unexclude(advancedGoals, "Tram Pass + Visit all 5 Tram Stations", "Mask Shard  in the Hive");
-            Exclude(advancedGoals, "Tram Pass + Visit all 5 Tram Stations", "Mask Shard  in the Hive", lineExlusion);
+            Unexclude(goals, "Tram Pass + Visit all 5 Tram Stations", "Hive Knight");
+            Unexclude(goals, "Tram Pass + Visit all 5 Tram Stations", "Hiveblood");
+            Unexclude(goals, "Tram Pass + Visit all 5 Tram Stations", "Mask Shard  in the Hive");
+            Exclude(goals, "Tram Pass + Visit all 5 Tram Stations", "Mask Shard  in the Hive", lineExlusion);
         }
 
         public static void Exclude(Dictionary<string, AdvancedGoal> goals, string goal1, string goal2, bool line = false)
@@ -91,6 +90,11 @@ namespace BingoAdvancedCustomGeneration
                 }
             }
             return goals;
+        }
+
+        public static Dictionary<string, AdvancedGoal> GetGoalsByGroupName(string groupName)
+        {
+            return goalGroups[groupName];
         }
     }
 }
